dd72c20e1406a66efc1befcd805d95244e6d764d
SpidySamurai <javierchiortiz@gmail>
2023-05-22 20:52:36 -0600

    Add ECMASCRIPT 6 Promises

---
+++ src/es6/5. Promises.js
@@ -0,0 +1,17 @@
+const promiseFunction = (condition) => {
+    return new Promise((resolve, reject) => {
+        if (condition) {
+            resolve("Promise resolved")
+        } else {
+            reject("Promise rejected")
+        }
+    })
+}
+
+promiseFunction(true)
+    .then(response => console.log(response))
+    .catch(error => console.log(error))
+
+promiseFunction(false)
+    .then(response => console.log(response))
+    .catch(error => console.log(error)) 
