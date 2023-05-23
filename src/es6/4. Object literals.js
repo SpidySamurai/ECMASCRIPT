fa2b67ece9cc7a5882fe14e7dc38a3334083ec63
SpidySamurai <javierchiortiz@gmail>
2023-05-22 20:32:18 -0600

    Add ECMASCRIPT 6 Object Literals

---
+++ src/es6/4. Object literals.js
@@ -0,0 +1,11 @@
+function newUser(user, age, country, uID) {
+    return {
+        user,
+        age,
+        country,
+        country,
+        uID
+    }
+}
+
+console.log(newUser('Spidy', 25, 'MX', 1))
